c568f39c22adc5a3e4773216251f72036f472904
muhammad yussuf <yussufm10@yahoo.com>
2016-06-10 01:09:36 +0700

    login database 09 juni2016

--- app/src/main/java/id/ac/unsyiah/elektro/mobile/bbcv2/DaftarActivity.java
+++ app/src/main/java/id/ac/unsyiah/elektro/mobile/bbcv2/DaftarActivity.java
@@ -1,6 +1,8 @@
 package id.ac.unsyiah.elektro.mobile.bbcv2;
 
+import android.app.Activity;
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
@@ -24,117 +26,75 @@ import id.ac.unsyiah.elektro.mobile.bbcv2.model.LoginDB;
 /**
  * Created by User on 6/8/2016.
  */
-public class DaftarActivity extends AppCompatActivity {
-    private static final String TAG = "SignupActivity";
+public class DaftarActivity extends Activity
+{
+    EditText editTextEmail,editTextPassword,editTextConfirmPassword;
+    Button btnCreateAccount;
 
-    @InjectView(R.id.input_name) EditText _nameText;
-    @InjectView(R.id.input_email) EditText _emailText;
-    @InjectView(R.id.input_password) EditText _passwordText;
-    @InjectView(R.id.btn_signup) Button _signupButton;
-    @InjectView(R.id.link_login) TextView _loginLink;
-
+    LoginDataBaseAdapter loginDataBaseAdapter;
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.daftaractivity);
-        ButterKnife.inject(this);
 
-        _signupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
+        // get Instance  of Database Adapter
+        loginDataBaseAdapter=new LoginDataBaseAdapter(this);
+        loginDataBaseAdapter=loginDataBaseAdapter.open();
+
+        // Get Refferences of Views
+        editTextEmail =(EditText)findViewById(R.id.editTextEmail);
+        editTextPassword=(EditText)findViewById(R.id.editTextPassword);
+        editTextConfirmPassword=(EditText)findViewById(R.id.editTextConfirmPassword);
+
+        btnCreateAccount=(Button)findViewById(R.id.buttonCreateAccount);
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+
+
             public void onClick(View v) {
-                signup();
+                // TODO Auto-generated method stub
+
+                String Email= editTextEmail.getText().toString();
+                String password=editTextPassword.getText().toString();
+                String confirmPassword=editTextConfirmPassword.getText().toString();
+
+
+
+                // check if any of the fields are vaccant
+                if(Email.equals("")||password.equals("")||confirmPassword.equals(""))
+                {
+                    Toast.makeText(getApplicationContext(), "Field Vaccant", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                // check if both password matches
+                if(!password.equals(confirmPassword))
+                {
+                    Toast.makeText(getApplicationContext(), "Password does not match", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                else
+                {
+                    // Save the Data in Database
+                    loginDataBaseAdapter.insertEntry(Email, password);
+                    Toast.makeText(getApplicationContext(), "Account Successfully Created ", Toast.LENGTH_LONG).show();
+                    Intent daftar=new Intent(getApplicationContext(),MainActivityLogin.class);
+                    startActivity(daftar);
+                    finish();
+                }
+
             }
+
         });
 
-        _loginLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Finish the registration screen and return to the Login activity
-                finish();
-            }
-        });
     }
 
-    public void signup() {
-        Log.d(TAG, "Signup");
-
-        if (!validate()) {
-            onSignupFailed();
-            return;
-        }
-
-        _signupButton.setEnabled(false);
-
-        final ProgressDialog progressDialog = new ProgressDialog(DaftarActivity.this,
-                R.style.AppTheme_Dark_Dialog);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Creating Account...");
-        progressDialog.show();
-
-        String name = _nameText.getText().toString();
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
 
 
+        loginDataBaseAdapter.close();
 
-
-
-        // TODO: Implement your own signup logic here.
-
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onSignupSuccess or onSignupFailed
-                        // depending on success
-                        onSignupSuccess();
-                        // onSignupFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
     }
-
-
-    public void onSignupSuccess() {
-        _signupButton.setEnabled(true);
-        setResult(RESULT_OK, null);
-        finish();
-    }
-
-    public void onSignupFailed() {
-        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
-
-        _signupButton.setEnabled(true);
-    }
-
-    public boolean validate() {
-        boolean valid = true;
-
-        String name = _nameText.getText().toString();
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
-
-        if (name.isEmpty() || name.length() < 3) {
-            _nameText.setError("at least 3 characters");
-            valid = false;
-        } else {
-            _nameText.setError(null);
-        }
-
-        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            _emailText.setError("enter a valid email address");
-            valid = false;
-        } else {
-            _emailText.setError(null);
-        }
-
-        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-            _passwordText.setError("between 4 and 10 alphanumeric characters");
-            valid = false;
-        } else {
-            _passwordText.setError(null);
-        }
-
-        return valid;
-    }
-
 }
---
+++ app/src/main/java/id/ac/unsyiah/elektro/mobile/bbcv2/MainActivityLogin.java
@@ -0,0 +1,97 @@
+package id.ac.unsyiah.elektro.mobile.bbcv2;
+
+/**
+ * Created by User on 6/9/2016.
+ */
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivityLogin extends Activity {
+    Button btnSignIn,btnSignUp;
+    LoginDataBaseAdapter loginDataBaseAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainactivitylogin);
+
+        // create a instance of SQLite Database
+        loginDataBaseAdapter=new LoginDataBaseAdapter(this);
+        loginDataBaseAdapter=loginDataBaseAdapter.open();
+
+        // Get The Refference Of Buttons
+        btnSignIn=(Button)findViewById(R.id.buttonSignIN);
+        btnSignUp=(Button)findViewById(R.id.buttonSignUP);
+
+        // Set OnClick Listener on SignUp button
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                /// Create Intent for SignUpActivity  abd Start The Activity
+                Intent intentSignUP=new Intent(getApplicationContext(),DaftarActivity.class);
+                startActivity(intentSignUP);
+            }
+        });
+    }
+    // Methos to handleClick Event of Sign In Button
+    public void signIn(View V)
+
+    {
+        final Dialog dialog = new Dialog(MainActivityLogin.this);
+        dialog.setContentView(R.layout.loginactivity);
+        dialog.setTitle("Login");
+
+        // get the Refferences of views
+        final EditText editTextEmail =(EditText)dialog.findViewById(R.id.editTextEmailToLogin);
+        final  EditText editTextPassword=(EditText)dialog.findViewById(R.id.editTextPasswordToLogin);
+
+        Button btnSignIn=(Button)dialog.findViewById(R.id.buttonSignIn);
+
+        // Set On ClickListener
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // get The User name and Password
+                String userName= editTextEmail.getText().toString();
+                String password=editTextPassword.getText().toString();
+
+                // fetch the Password form database for respective user name
+                String storedPassword=loginDataBaseAdapter.getSinlgeEntry(userName);
+
+
+                // check if the Stored password matches with  Password entered by user
+                if(password.equals(storedPassword))
+                {
+                    Toast.makeText(MainActivityLogin.this, "Congrats: Login Successfull", Toast.LENGTH_LONG).show();
+                    dialog.dismiss();
+
+                    Intent masuk=new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(masuk);
+                    finish();
+                }
+                else
+                {
+                    Toast.makeText(MainActivityLogin.this, "Email or Password does not match", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        dialog.show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Close The Database
+        loginDataBaseAdapter.close();
+    }
+}
